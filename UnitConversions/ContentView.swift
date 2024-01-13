3e15ff9ca800f852f9782f1717cf7a471313230c
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2024-01-13 21:49:39 +0300

    Create bacis UI.

--- UnitConversions/ContentView.swift
+++ UnitConversions/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedInput = "meters"
+    @State private var selectedOuput = "meters"
+    @State private var inputValue = 0.0
+    
+    let lenghtConversion = ["meters", "kilometers", "feet", "yards", "miles"]
+    
+    var conversionResult: Double {
+        return 0.0
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form {
+            Section("Choose an imput unit") {
+                Picker("Input unit", selection: $selectedInput) {
+                    ForEach(lenghtConversion, id: \.self) {
+                        Text("\($0)")
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            
+            Section("Choose an output unit") {
+                Picker("Output value", selection: $selectedOuput) {
+                    ForEach(lenghtConversion, id: \.self) {
+                        Text("\($0)")
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            
+            Section("Enter a value") {
+                TextField("Input value", value: $inputValue, format: .number)
+            }
+            
+            Section("Result of the conversion") {
+                Text(conversionResult, format: .number)
+            }
+            
         }
-        .padding()
     }
 }
 
