0987e97dec7cc40ab09478a279a1d735e054491b
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2024-01-15 11:07:38 +0300

    Add a logic of conversion.

--- UnitConversions/ContentView.swift
+++ UnitConversions/ContentView.swift
@@ -15,7 +15,40 @@ struct ContentView: View {
     let lenghtConversion = ["meters", "kilometers", "feet", "yards", "miles"]
     
     var conversionResult: Double {
-        return 0.0
+        var inputMeters = 0.0
+        var output = 0.0
+        
+        switch selectedInput {
+        case "meters":
+            inputMeters = inputValue
+        case "kilometers":
+            inputMeters = inputValue * 1000
+        case "feet":
+            inputMeters = inputValue * 0.3048
+        case "yards":
+            inputMeters = inputValue * 0.9144
+        case "miles":
+            inputMeters = inputValue * 1609.34
+        default:
+            inputMeters = 0.0
+        }
+        
+        switch selectedOuput {
+        case "meters":
+            output = inputMeters
+        case "kilometers":
+            output = inputMeters / 1000
+        case "feet":
+            output = inputMeters / 0.3048
+        case "yards":
+            output = inputMeters / 0.9144
+        case "miles":
+            output = inputMeters / 1609.34
+        default:
+            output = 0.0
+        }
+        
+        return output
     }
     
     var body: some View {
@@ -43,7 +76,7 @@ struct ContentView: View {
             }
             
             Section("Result of the conversion") {
-                Text(conversionResult, format: .number)
+                Text(conversionResult.formatted())
             }
             
         }
