caea92802639796584cd448ee52028989442157d
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2024-05-02 10:59:00 +0300

    Add the base UI.

--- UnitConversions/ContentView.swift
+++ UnitConversions/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var inputUnit = "meters"
+    @State private var outputUnit = "meters"
+    @State private var inputValue = 0.0
+    
+    let units = ["meters", "kilometers", "feet", "yards", "miles"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section("Choose an input unit") {
+                    Picker("Input unit", selection: $inputUnit) {
+                        ForEach(units, id: \.self) {
+                            Text("\($0)")
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section("Choose an output unit") {
+                    Picker("Output unit", selection: $outputUnit) {
+                        ForEach(units, id: \.self) {
+                            Text("\($0)")
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section("Enter a value") {
+                    TextField("Input value", value: $inputValue, format: .number)
+                        .keyboardType(.decimalPad)
+                }
+                
+                Section("Result of conversion") {
+                    Text("")
+                }
+            }
+            .navigationTitle("Unit Conversions")
         }
-        .padding()
     }
 }
 
